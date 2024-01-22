0fb55d4a7c7a01ce11795833c1d4247104a0b483
Jordan Delgado <jordindincito@gmail.com>
2024-01-22 13:00:16 -0500

    spring course, 2 & 3 code topic updated

--- 1-java-springboot3/3-spring-core-ioc/SpringDemo/src/main/java/com/shordan/SpringDemo/App.java
+++ 1-java-springboot3/3-spring-core-ioc/SpringDemo/src/main/java/com/shordan/SpringDemo/App.java
@@ -1,18 +1,21 @@
 package com.shordan.SpringDemo;
 
 import org.springframework.beans.factory.BeanFactory;
+
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+
 import org.springframework.core.io.FileSystemResource;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+    	
         Alien obj = (Alien) factory.getBean("alien");
         obj.code();
     }
